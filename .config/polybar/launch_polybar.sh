98684bef3e13fe44ef6c5eb719b5b34ba2aab71e
Paula Vaccaro <paulavaccaro1@gmail.com>
2024-11-23 12:04:19 -0300

    add(polybar): add polybar with modules

---
+++ .config/polybar/launch_polybar.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+i=0
+if type "xrandr"; then
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  let i++
+  MONITOR=$m polybar --reload mon$i &
+  echo $m
+  echo mon$i
+done
+else
+ polybar --reload mon1 &
+#  polybar --reload mon2 &
+fi
